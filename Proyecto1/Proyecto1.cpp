66f8189a99dd5f7a09544ebedc2d688ddf473667
BernardoG <A00570682@itesm.mx>
2020-03-04 19:05:44 -0600

    Dia 4. Proyecto Finalizado. Nota: Despues del commit anterior. Todo se reinicio.

--- Proyecto1/Proyecto1.cpp
+++ Proyecto1/Proyecto1.cpp
@@ -6,25 +6,15 @@ using namespace std;
 
 int main() 
 {
-	string ae, as, line, x, y, oper, op, z;
-	int i = 0, j = 0, n, numero;
-	int num[100];
+	string ae, as, line, x, y, oper, n1, n2;
+	int i = 0, esp, num1, num2, result;
+	char arrcod[15];
 
-	cout << "Ingrese el nombre del archivo de texto de entrada (incluyendo extension .txt ) : " << endl;
+	cout << "Ingrese el nombre del archivo de texto de entrada : " << endl;
 	cin >> ae;
 
-	cout << "Ingrese el nombre del archivo de texto de salida (incluyendo extension .txt ) : " << endl;
+	cout << "Ingrese el nombre del archivo de texto de salida : " << endl;
 	cin >> as;
-
-	ofstream archivoe;
-	archivoe.open(ae);
-	archivoe << "B y / 8 T & Q E P F C c # @" << endl;
-	archivoe << "y/8B @ yB" << endl;
-	archivoe << "yBBB # /" << endl;
-	archivoe << "TFPyy C &QQy" << endl;
-	archivoe << "8EPPF8/ c /" << endl;
-
-	archivoe.close();
 
 	ifstream archivo2;
 	archivo2.open(ae);
@@ -32,244 +22,72 @@ int main()
 	ofstream archivosal;
 	archivosal.open(as);
 	
-	while (getline(archivo2,x))
-	{		
-		while (i <= x.length())
-		{			
-			switch (x[i])
-			{
-			case'B':
-				line = '0';
-				break;
+	getline(archivo2, y);
+	cout << endl;
 
-			case 'y':
-				line = '1';
-				break;
-
-			case '/':
-				line = '2';
-				break;
-
-			case '8':
-				line = '3';
-				break;
-
-			case 'T':
-				line = '4';
-				break;
-
-			case '&':
-				line = '5';
-				break;
-
-			case 'Q':
-				line = '6';
-				break;
-
-			case 'E':
-				line = '7';
-				break;
-
-			case 'P':
-				line = '8';
-				break;
-
-			case 'F':
-				line = '9';
-				break;
-
-			case 'C':
-				oper = '+';
-				break;
-
-			case 'c':
-				oper = '-';
-				break;
-
-			case '#':
-				oper = '*';
-				break;
-
-			case '@':
-				oper = '/';
-				break;
-			
-			case ' ':
-				x.erase(i, 1);
-				i--;
-				break;
-			}
-
-			numero = stoi(line);
-
-			if (i<20)
-			{
-				archivosal << numero;
-				std::cout << numero;
-			}
-			else
-			{
-				archivosal << oper;
-				std::cout << oper;
-			}
-				
-			
-			i++;
-			i++;
-		}
-		
-		archivosal << endl;
-		cout << endl;
-				
-	}
-
-	while (archivo2 >> y >> op >> z)
+	while (i< y.length())
 	{
-		switch (y[i])
-		{
-		case'B':
-			line = '0';
-			break;
-
-		case 'y':
-			line = '1';
-			break;
-
-		case '/':
-			line = '2';
-			break;
-
-		case '8':
-			line = '3';
-			break;
-
-		case 'T':
-			line = '4';
-			break;
-
-		case '&':
-			line = '5';
-			break;
-
-		case 'Q':
-			line = '6';
-			break;
-
-		case 'E':
-			line = '7';
-			break;
-
-		case 'P':
-			line = '8';
-			break;
-
-		case 'F':
-			line = '9';
-			break;
-
-		case 'C':
-			oper = '+';
-			break;
-
-		case 'c':
-			oper = '-';
-			break;
-
-		case '#':
-			oper = '*';
-			break;
-
-		case '@':
-			oper = '/';
-			break;
-
-		case ' ':
-			y.erase(i, 1);
-			i--;
-			break;
-		}
-		switch (z[i])
-		{
-		case'B':
-			line = '0';
-			break;
-
-		case 'y':
-			line = '1';
-			break;
-
-		case '/':
-			line = '2';
-			break;
-
-		case '8':
-			line = '3';
-			break;
-
-		case 'T':
-			line = '4';
-			break;
-
-		case '&':
-			line = '5';
-			break;
-
-		case 'Q':
-			line = '6';
-			break;
-
-		case 'E':
-			line = '7';
-			break;
-
-		case 'P':
-			line = '8';
-			break;
-
-		case 'F':
-			line = '9';
-			break;
-
-		case 'C':
-			oper = '+';
-			break;
-
-		case 'c':
-			oper = '-';
-			break;
-
-		case '#':
-			oper = '*';
-			break;
-
-		case '@':
-			oper = '/';
-			break;
-
-		case ' ':
-			z.erase(i, 1);
-			i--;
-			break;
-		}
-
-		cout << y << op << z << endl;
-
-		
+		arrcod[i] = y[i];
+		cout << arrcod[i];
+		i++;
+		y.erase(i, 1);
 	}
 
-	
+	cout << endl;
+	i = 0;
+	 
+	while (getline(archivo2,x))
+	{
+		while (i < x.length())
+		{
+			if (x[i] == arrcod[0]){ x[i] = '0';}
+			else if (x[i] == arrcod[1]) { x[i] = '1';}
+			else if (x[i] == arrcod[2]) { x[i] = '2'; }
+			else if (x[i] == arrcod[3]) { x[i] = '3'; }
+			else if (x[i] == arrcod[4]) { x[i] = '4'; }
+			else if (x[i] == arrcod[5]) { x[i] = '5'; }
+			else if (x[i] == arrcod[6]) { x[i] = '6'; }
+			else if (x[i] == arrcod[7]) { x[i] = '7'; }
+			else if (x[i] == arrcod[8]) { x[i] = '8'; }
+			else if (x[i] == arrcod[9]) { x[i] = '9'; }
+			else if (x[i] == arrcod[10]) { x[i] = '+'; }
+			else if (x[i] == arrcod[11]) { x[i] = '-'; }
+			else if (x[i] == arrcod[12]) { x[i] = '*'; }
+			else if (x[i] == arrcod[13]) { x[i] = '/'; }
+			else { x[i] = ' '; }
+
+			cout << x[i];
+			archivosal << x[i];
+						
+			i++;
+		}
+
+		
+
+		esp = x.find(' ');
+		n1 = x.substr(0, esp);
+		n2 = x.substr(esp+2, x.length());
+
+		num1 = stoi(n1);
+		num2 = stoi(n2);
+
+		if (x.find('+') != -1) { result = num1 + num2; }
+		else if (x.find('-') != -1) { result = num1 - num2; }
+		else if (x.find('*') != -1) { result = num1 * num2; }
+		else if (x.find('/') != -1) { result = num1 / num2; }
+		
+		cout << " = " << result << endl;
+		archivosal << " = " << result << endl;
+
+		
+		i = 0;
+
+	}
+
 	archivosal.close();
 
 	archivo2.close();
 
-
-	
-
-
-
-
-	
-	
-	
+	   	 	
 
 }
